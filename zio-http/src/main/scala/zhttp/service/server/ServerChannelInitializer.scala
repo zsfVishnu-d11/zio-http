e13a6e4fbd9a4e81427e7aaeb24c0557caf52a8b
test <amit.singh@dream11.com>
2021-05-20 11:05:13 +0530

    previous commit 2,843,535.15 Req/sec, remove KeepAliveHandler

--- zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
@@ -3,7 +3,6 @@ package zhttp.service.server
 import io.netty.handler.codec.http.{
   HttpRequestDecoder => JHttpRequestDecoder,
   HttpResponseEncoder => JHttpResponseEncoder,
-  HttpServerKeepAliveHandler => JHttpServerKeepAliveHandler,
 }
 import io.netty.handler.flush.{FlushConsolidationHandler => JFlushConsolidationHandler}
 import zhttp.core._
@@ -20,7 +19,7 @@ final case class ServerChannelInitializer(httpH: JChannelHandler, maxSize: Int) 
       .addLast(FLUSH_CONSOLIDATOR, new JFlushConsolidationHandler(256, false))
       .addLast(SERVER_ENCODER, new JHttpResponseEncoder)
       .addLast(SERVER_DECODER, new JHttpRequestDecoder(4096, 8192, 8192, false))
-      .addLast(HTTP_KEEPALIVE_HANDLER, new JHttpServerKeepAliveHandler)
+      //.addLast(HTTP_KEEPALIVE_HANDLER, new JHttpServerKeepAliveHandler)
       .addLast(OBJECT_AGGREGATOR, new JHttpObjectAggregator(maxSize))
       .addLast(HTTP_REQUEST_HANDLER, httpH)
     ()
