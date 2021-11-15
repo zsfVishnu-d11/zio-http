7f580ac7837f8923a29df5bffc108467c1c73938
amitsingh <amitksingh1490@gmail.com>
2021-11-15 10:05:14 +0530

    feat(try): revert timer service

--- zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
@@ -5,12 +5,11 @@ import io.netty.handler.codec.http.{HttpHeaderNames, HttpHeaders, HttpResponse}
 import java.text.SimpleDateFormat
 import java.util.Date
 
-class ServerTimeGenerator() {
-  private val formatter                     = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z")
-  @volatile
-  var date                                  = new Date()
+case class ServerTimeGenerator() {
+  private val formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z")
+
   def update(headers: HttpHeaders): Boolean = {
-    headers.set(HttpHeaderNames.DATE, formatter.format(date))
+    headers.set(HttpHeaderNames.DATE, formatter.format(new Date()))
     true
   }
 
@@ -22,12 +21,6 @@ class ServerTimeGenerator() {
 
 object ServerTimeGenerator {
   def make: ServerTimeGenerator = {
-    new ServerTimeGenerator() { self =>
-      val t    = new java.util.Timer()
-      val task = new java.util.TimerTask {
-        def run() = self.date = new Date()
-      }
-      t.schedule(task, 0, 1000L)
-    }
+    new ServerTimeGenerator()
   }
 }
